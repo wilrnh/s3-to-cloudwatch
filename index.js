d9b70ee9d941f4f8219aa45b3b17e40bfbeae192
wilrnh <wilrnh@msn.com>
2016-02-24 21:14:17 -0500

    added bucket name to dimensions

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ exports.handler = function(event, context) {
   
   var s3_event = event.Records[0].eventName;
   
+  var s3_bucket = event.Records[0].s3.bucket.name;
+  
   var s3_key = event.Records[0].s3.object.key;
   
   var s3_key_parts = s3_key.split('/');
@@ -14,15 +16,21 @@ exports.handler = function(event, context) {
   var metric = {
     MetricData: [{
       MetricName: s3_event,
-      Dimensions: [{
-        Name: 'Key',
-        Value: s3_key_parts[0]
-      }],
+      Dimensions: [
+        {
+          Name: 'Key',
+          Value: s3_key_parts[0]
+        },
+        {
+          Name: 'Bucket',
+          Value: s3_bucket
+        }
+      ],
       Timestamp: s3_event_timestamp,
       Unit: 'Count',
       Value: 1
     }],
-    Namespace: 'S3Cloudwatch'
+    Namespace: 's3-to-cloudwatch'
   };
   
   CloudWatch.putMetricData(metric, context.done);

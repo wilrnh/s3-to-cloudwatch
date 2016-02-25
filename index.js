c7750849c533bf0ab392530d0888c54827707b8a
wilrnh <wilrnh@msn.com>
2016-02-24 21:19:42 -0500

    added metric to track all keys

--- index.js
+++ index.js
@@ -14,22 +14,40 @@ exports.handler = function(event, context) {
   var s3_key_parts = s3_key.split('/');
   
   var metric = {
-    MetricData: [{
-      MetricName: s3_event,
-      Dimensions: [
-        {
-          Name: 'Key',
-          Value: s3_key_parts[0]
-        },
-        {
-          Name: 'Bucket',
-          Value: s3_bucket
-        }
-      ],
-      Timestamp: s3_event_timestamp,
-      Unit: 'Count',
-      Value: 1
-    }],
+    MetricData: [
+      {
+        MetricName: s3_event,
+        Dimensions: [
+          {
+            Name: 'Key',
+            Value: s3_key_parts[0]
+          },
+          {
+            Name: 'Bucket',
+            Value: s3_bucket
+          }
+        ],
+        Timestamp: s3_event_timestamp,
+        Unit: 'Count',
+        Value: 1
+      },
+      {
+        MetricName: s3_event,
+        Dimensions: [
+          {
+            Name: 'Key',
+            Value: '*'
+          },
+          {
+            Name: 'Bucket',
+            Value: s3_bucket
+          }
+        ],
+        Timestamp: s3_event_timestamp,
+        Unit: 'Count',
+        Value: 1
+      }
+    ],
     Namespace: 's3-to-cloudwatch'
   };
   
